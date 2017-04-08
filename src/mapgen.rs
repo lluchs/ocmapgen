e995bda87d8a2c8127e62a446786ad5dbefe9984
Lukas Werling <lukas.werling@gmail.com>
2017-04-08 20:55:58 +0200

    Remove debug println

--- src/mapgen.rs
+++ src/mapgen.rs
@@ -208,7 +208,6 @@ impl<'a> MapGenHandle<'a> {
                                     ((color >> 8) & 0xff) as u8,
                                     (color & 0xff) as u8] }
             } else {
-                println!("no texture name for {}", mat_idx);
                 // This really shouldn't happen and I think mape doesn't even handle this case.
                 SKY
             }
